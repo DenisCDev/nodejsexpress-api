cfbfb82996dd71c4afe216ca2111fe13c76a27a3
IntLab4 <intlabs@proton.me>
2023-11-04 12:27:55 -0300

    feat: adicionando model para personagem rpg na api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-const express = require("express")
+const express = require("express");
+const moongose = require("mongoose");
+
 const app = express()
 const port = 2000
+mongoose.connect('mongodb+srv://denisscarabelli:<password>@api-rpg.sc7thba.mongodb.net/?retryWrites=true&w=majority');
 
-app.get('/', (req, res) => {
+const Personagem = mongoose.model('Personagem', {
+    nome: String,
+    idade: Number,
+    classe: String,
+    nivel: Number,
+    raca: String,
+    antecedente: String,
+    alinhamento: String,
+    imagemPersonagem: String
+});
+
+app.get("/", (req, res) => {
     res.send('Começando novo projeto node com express')
+})
+
+app.post("/", (req, res) => {
+    const film = new Film({
+        nome: req.body.nome,
+        idade: req.body.idade,
+        classe: req.body.classe,
+        nivel: req.body.nivel,
+        raca: req.body.raca,
+        antecedente: req.body.antecedente,
+        alinhamento: req.body.alinhamento,
+        imagemPersonagem: req.body.imagemPersonagem,
+    })
 })
 
 app.listen(port, () => {
